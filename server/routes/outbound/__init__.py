eea7c765427247d03e321abc1f1f8ac56a582d3a
root <root@DESKTOP-RJR2G8T.localdomain>
2022-04-21 01:39:48 +0500

    Finalized app

--- server/routes/outbound/__init__.py
+++ server/routes/outbound/__init__.py
@@ -7,8 +7,6 @@ from wtforms.validators import DataRequired
 from flask_inputs import Inputs
 from server.models import PhoneNumber
 
-
-
 class OutboundInputs(Inputs):
     json = {
         'from': [DataRequired(message="from is missing"),param_validation],
@@ -26,32 +24,36 @@ class OutboundAPI(MethodResource,Resource):
         response["error"] = ""
         request_obj = request.get_json()
         req = OutboundInputs(request)
-        if(req.validate()):
-            
-            check_from = PhoneNumber.query.filter_by(account_id=current_account_id, number=request_obj["from"]).first()
-            if(check_from):
-                hash = f"STOP_{request_obj['to']}:{request_obj['from']}"
-                cache_entry = redis_client.hget(hash,request_obj["to"])
-                if(cache_entry):
-                    response["error"] =  f"sms from {request_obj['from']} to {request_obj['to']} blocked by STOP request"
-                else:
-                    hash = f"LIMIT_{request_obj['from']}"
-                    if(redis_client.hsetnx(hash,request_obj["from"],50)):
-                        redis_client.expire(hash, 24*60*60)
-                    current_limit_value = redis_client.hget(hash,request_obj["from"])
-                    if(current_limit_value and int(current_limit_value)>0): 
-                        print("previous_count",str(current_limit_value))
-                        updated_limit_value = int(current_limit_value) - 1
-                        redis_client.hset(hash,request_obj["from"],updated_limit_value)
-                        response["message"] = "outbound sms ok"
+        try:
+            if(req.validate()):
+                check_from = PhoneNumber.query.filter_by(account_id=current_account_id, number=request_obj["from"]).first()
+                if(check_from):
+                    hash = f"STOP_{request_obj['to']}:{request_obj['from']}"
+                    cache_entry = redis_client.hget(hash,request_obj["to"])
+                    if(cache_entry):
+                        response["error"] =  f"sms from {request_obj['from']} to {request_obj['to']} blocked by STOP request"
                     else:
-                        response["message"] = ""
-                        response["error"] = f"limit reached for from {request_obj['from']}"
-                        status_code = 429
+                        hash = f"LIMIT_{request_obj['from']}"
+                        if(redis_client.hsetnx(hash,request_obj["from"],50)):
+                            redis_client.expire(hash, 24*60*60)
+                        current_limit_value = redis_client.hget(hash,request_obj["from"])
+                        if(current_limit_value and int(current_limit_value)>0): 
+                            #print("previous_count",str(current_limit_value))
+                            updated_limit_value = int(current_limit_value) - 1
+                            redis_client.hset(hash,request_obj["from"],updated_limit_value)
+                            response["message"] = "outbound sms ok"
+                        else:
+                            response["message"] = ""
+                            response["error"] = f"limit reached for from {request_obj['from']}"
+                            status_code = 429
+                else:
+                    status_code = 404
+                    response["error"] = "from parameter not found"
             else:
-                status_code = 404
-                response["error"] = "from parameter not found"
-        else:
-            response["error"] = (req.errors)
-            status_code = 400
-        return make_response(response,status_code)
+                response["error"] = (req.errors)
+                status_code = 400
+        except :
+            status_code = 500
+            response["error"] = "unknown failure" 
+        finally:
+            return make_response(response,status_code)
